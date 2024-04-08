6b7a864b7d43039ce34d3c099b8ccdd727ebf27f
DamnDaniel <144451199+DamnDaniel126030@users.noreply.github.com>
2024-04-08 15:49:12 +0200

    Update BankTest.cs

--- UnitTestProject/BankTest.cs
+++ UnitTestProject/BankTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +9,55 @@ namespace UnitTestProject
 {
 	internal class BankTest
 	{
+		Bank b;
+
+		[SetUp]
+		public void Setup()
+		{
+			b = new Bank();
+			b.UjSzamla("Gipsz Jakab", "1234");
+		}
+
 		[Test]
 		public void UjSzamlaEgyenleg0()
 		{
-			Bank b = new Bank();
-			b.UjSzamla("Gipsz Jakab", "1234");
 			Assert.That(b.Egyenleg("1234"), Is.Zero);
+		}
+
+		[Test]
+		public void UjSzamlaUresNev()
+		{
+			Assert.Throws<ArgumentException>(() => b.UjSzamla("", "1234"));
+		}
+
+		[Test]
+		public void UjSzamlaUresSzamlaszam()
+		{
+			Assert.Throws<ArgumentException>(() => b.UjSzamla("Teszt Elek", ""));
+		}
+
+		[Test]
+		public void UjSzamlaNullNev()
+		{
+			Assert.Throws<ArgumentNullException>(() => b.UjSzamla(null, "1234"));
+		}
+
+		[Test]
+		public void UjSzamlaNullSzamlaszam()
+		{
+			Assert.Throws<ArgumentNullException>(() => b.UjSzamla("Teszt Elek", null));
+		}
+
+		[Test]
+		public void UjSzamlaLetezoSzamlaszam()
+		{
+			Assert.Throws<ArgumentException>(() => b.UjSzamla("Teszt Elek", "1234"));
+		}
+
+		[Test]
+		public void UjSzamlaLetezoNev()
+		{
+			Assert.DoesNotThrow(() => b.UjSzamla("Gipsz Jakab", "5678"));
 		}
 	}
 }
